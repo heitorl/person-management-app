847e3579374c7a714f25f937c0993f26557bb409
heitorl <juniorio01@hotmail.com>
2024-10-24 03:13:22 -0300

    feat: override find controller to search cities by state via external API

--- backend/src/api/cidade/controllers/cidade.ts
+++ backend/src/api/cidade/controllers/cidade.ts
@@ -2,6 +2,27 @@
  * cidade controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::cidade.cidade');
+export default factories.createCoreController(
+  "api::cidade.cidade",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const state = ctx.request.headers["estado"];
+      if (state) {
+        const response = await fetch(
+          `http://educacao.dadosabertosbr.org/api/cidades/${state}`
+        );
+        const citiesData = (await response.json()) as string[];
+
+        const cities = citiesData.map((city) => city.split(":")[1]);
+
+        return cities;
+      }
+
+      const { data, meta } = await super.find(ctx);
+
+      return { data, meta };
+    },
+  })
+);
--- backend/src/api/estado/controllers/estado.ts
+++ backend/src/api/estado/controllers/estado.ts
@@ -2,6 +2,6 @@
  * estado controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::estado.estado');
+export default factories.createCoreController("api::estado.estado");
